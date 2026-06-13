4f004d8a81688f3f7b712a752a30805cdf100f48
tatancho <81977587+tatancho@users.noreply.github.com>
2026-06-13 17:51:12 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
-import Landing from './Landing.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/book/:bookId" element={<App />} />
-      </Routes>
-    </HashRouter>
+    <App />
   </React.StrictMode>,
 )
